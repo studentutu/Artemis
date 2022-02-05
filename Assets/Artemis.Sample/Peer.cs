cf9627893cb6d844530d8178993ef7fce7f2409f
Gustavo Santos <gustappsantos@gmail.com>
2022-02-05 17:31:21 -0300

    Puts sample in a dedicated assembly

---
+++ Assets/Artemis.Sample/Peer.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+using Artemis.Clients;
+using Artemis.ValueObjects;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace Artemis.Sample
+{
+    public class Peer : MonoBehaviour
+    {
+        private Address _recipient;
+        private ArtemisClient _client;
+        [SerializeField] private Peer _other;
+
+        private readonly List<Handler> _handlers = new()
+        {
+            new DateRequestHandler(),
+            new VehicleMessageHandler(),
+        };
+
+        private void Awake()
+        {
+            _client = new ArtemisClient(_handlers);
+            _client.Start();
+        }
+
+        private void Start()
+        {
+            _recipient = Address.FromHostname("localhost", _other._client.Port);
+        }
+
+        private void OnDestroy()
+        {
+            _client.Dispose();
+        }
+
+        public void Present()
+        {
+            using (new GUILayout.VerticalScope("box", GUILayout.Width(160)))
+            {
+                GUILayout.Label(gameObject.name, new GUIStyle("Label") {alignment = TextAnchor.MiddleCenter});
+                if (GUILayout.Button("Request")) Request();
+                if (GUILayout.Button("Send Reliable")) SendReliable();
+                if (GUILayout.Button("Send Unreliable")) SendUnreliable();
+                if (GUILayout.Button("Mass Send Reliable")) MassSendReliable();
+            }
+        }
+
+        private async void Request()
+        {
+            Debug.Log(await _client.Request(new DateTime(), _recipient));
+        }
+
+        private void SendReliable()
+        {
+            _client.SendMessage(new Vehicle("Honda"), _recipient, DeliveryMethod.Reliable);
+        }
+
+        private void SendUnreliable()
+        {
+            _client.SendMessage(new Vehicle("Honda"), _recipient, DeliveryMethod.Unreliable);
+        }
+
+        private void MassSendReliable()
+        {
+            for (int i = 0; i < 1024; i++)
+            {
+                _client.SendMessage(new Vehicle("Honda"), _recipient, DeliveryMethod.Reliable);
+            }
+        }
+    }
+}
---
+++ Assets/Artemis.Sample/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Artemis.Sample
+{
+    [Serializable]
+    public readonly struct Vehicle
+    {
+        public readonly string Brand;
+
+        public Vehicle(string brand)
+        {
+            Brand = brand;
+        }
+    }
+}
