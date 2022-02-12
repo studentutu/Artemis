6fde24d45eed8da90bbc2d84fb059d05092f65c4
Gustavo Santos <gustappsantos@gmail.com>
2022-02-12 08:29:04 -0300

    Adds PlayerView prefab

---
+++ Assets/Artemis.Sample/Player/PlayerView.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    [field: SerializeField] private TMP_Text _nickname;
+    [field: SerializeField] private SpriteRenderer _background;
+    [field: SerializeField] private SpriteRenderer _foreground;
+}
