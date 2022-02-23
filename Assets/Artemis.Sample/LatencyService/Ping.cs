9fffb70fa29c61f944acc9f113afb9f9a23f9f6c
Gustavo Santos <gustappsantos@gmail.com>
2022-02-23 18:44:28 -0300

    Moves sample to a dedicated repository

--- Assets/Artemis.Sample/LatencyService/Ping.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-[Serializable]
-public struct Ping
-{
-}
--- Assets/Artemis.Sample/Player/PlayerView.cs
+++
@@ -1,8 +0,0 @@
-using TMPro;
-using UnityEngine;
-
-public class PlayerView : MonoBehaviour
-{
-    [field: SerializeField] public TMP_Text Nickname { get; private set; }
-    [field: SerializeField] public SpriteRenderer Sprite { get; private set; }
-}
