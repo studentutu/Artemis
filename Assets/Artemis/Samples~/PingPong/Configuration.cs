b19b593362e4a8893e82ee7f790a004ef62cdbf7
Gustavo Santos <gustappsantos@gmail.com>
2022-03-03 13:19:56 -0300

    Reduces ping rate

--- Assets/Artemis/Samples~/PingPong/Configuration.cs
+++ Assets/Artemis/Samples~/PingPong/Configuration.cs
@@ -5,6 +5,7 @@ namespace Artemis.Samples.PingPong
     internal static class Configuration
     {
         internal const int ServerPort = 12345;
-        internal static readonly TimeSpan PingInterval = TimeSpan.FromSeconds(1f / 60);
+        private const int PingRate = 2;
+        internal static readonly TimeSpan PingInterval = TimeSpan.FromSeconds(1f / PingRate);
     }
 }
